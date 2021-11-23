18b9e7809cadf0c43ae82a4b66fb6b445a30dfbb
FrederikALP <fred05k3@stud.kea.dk>
2021-11-23 15:22:02 +0100

    html and js for summoners

--- src/main/resources/templates/summoners/summoners.js
+++ src/main/resources/templates/summoners/summoners.js
@@ -1,5 +1,58 @@
-fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner.name + "?api_key=" + apikey)
+const summonerDiv = document.getElementById("summoner-wrapper");
+const searchSummonerInput = document.getElementById("summoner-name");
+
+fetch(localurl + "/summoners")
     .then(response => response.json())
-    .then(result => {
-        console.log(result);
-    });
+    .then(summoners => {
+        summoners.map(addSummonerToDiv);
+    })
+
+function addSummonerToDiv(summoner){
+    const selectSummonerToDiv = document.createElement("div");
+    summonerDiv.appendChild(selectSummonerToDiv);
+    constructSummoner(selectSummonerToDiv, summoner);
+}
+
+function constructSummoner(divElement, summoner){
+    divElement.innerHTML = `
+    <a href="./summonersMatches.html?summonerId=${summoner.id}">
+    <h1>
+    ${escapeHTML(summoner.name)}
+    </h1>
+    </a>
+    `;
+}
+
+document.getElementById("search-for-summoner").addEventListener("click", searchForSummoner);
+
+function searchForSummoner(){
+    fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchSummonerInput.value + "?api_key=" + apikey)
+        .then(response => response.json())
+        .then(summoner => {
+           saveSummoner(summoner)
+        })
+};
+
+function saveSummoner(summoner){
+    const summonerToSave = {
+        summonerId: summoner.id,
+        accountId: summoner.accountId,
+        puuId: summoner.puuid,
+        name: summoner.name,
+        profileIconId: summoner.profileIconId,
+        summonerLevel: summoner.summonerLevel
+    };
+
+    fetch(localurl + "/summoners", {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(summonerToSave)
+    }).then(response => {
+        if (response.status === 200) {
+            addSummonerToDiv(summonerToSave);
+        } else {
+            console.log("summoner not created", respone.status);
+        }
+    })
+        .catch(error => console.log("network error" + error));
+}
