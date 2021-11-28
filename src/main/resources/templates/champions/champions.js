e9a7d1f772d59edd0e2451dd46fe3ee7c13f001b
FrederikALP <fred05k3@stud.kea.dk>
2021-11-28 18:21:18 +0100

    champions but im kinda confused smile

--- src/main/resources/templates/champions/champions.js
+++ src/main/resources/templates/champions/champions.js
@@ -1,5 +1,56 @@
-fetch("https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/" + encrypted.summmonerid + "?api_key=" + apikey)
+const championDiv = document.getElementById("champion-wrapper-wrapper");
+const searchChampionInput = document.getElementById("champion-name-input");
+
+document.getElementById("search-for-champion").addEventListener("click", searchForChampion);
+
+function searchForChampion(){
+    fetch("http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json")
+        .then(response => response.json())
+        .then(champion => {
+            if (champion.data === searchChampionInput)
+                console.log(searchChampionInput)
+            })
+        };
+
+/*
+fetch(localurl + "/champions")
     .then(response => response.json())
-    .then(result => {
-        console.log(result);
-    });
+    .then(champions => {
+        champions.map(addChampionsToDiv);
+    })
+
+function addChampionToDiv(champion){
+    const selectChampionToDiv = document.createElement("div");
+    selectChampionToDiv.id=champion.id;
+    championDiv.appendChild(selectChampionToDiv);
+}
+
+document.getElementById("search-for-summoner").addEventListener("click", searchForChampion);
+
+function searchForChampion() {
+    fetch("http://ddragon.leagueoflegends.com/cdn/11.22.1/data/en_US/champion.json")
+        .then(response => response.json())
+        .then(champions => {
+            console.log(champions)
+        })
+};
+
+function saveChampion(champion){
+    const championToSave = {
+        championId: champion.id,
+        championLvl: champion.championLvl,
+        championName: champion.championName,
+        championIconId: champion.championIconId
+    };
+
+    fetch(localurl + "/champions", {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+        body: JSON.stringify(championToSave)
+    }).then(response => response.json())
+        .then(champion => {
+            addChampionToDiv(champion);
+        })
+        .catch(error => console.log("network error" + error));
+}
+*/
